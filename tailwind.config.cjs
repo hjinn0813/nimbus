0ca51607876da934d23f0dead19d1e1d3e1d9dbc
hjinn0813 <hjc3790@gmail.com>
2025-06-01 14:45:55 +0900

    feat: 컬러 설정 추가

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,13 @@ module.exports = {
       fontFamily: {
         sans: ['Pretendard', 'sans-serif'],
       },
+      colors: {
+        main_color: '#16a34a',      // emerald-600
+        sub_color: '#84cc16',       // lime-500
+        error: '#f43f5e',           // rose-400
+        text_primary: '#1e293b',    // slate-800
+        text_secondary: '#6b7280'   // slate-500
+      }      
     },
   },
   plugins: [],
