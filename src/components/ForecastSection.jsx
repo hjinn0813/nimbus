307221333b76d9527ae75fdd1c9440959aae0022
hjinn0813 <hjc3790@gmail.com>
2025-07-26 18:29:25 +0900

    feat: 디테일 페이지 기본구조 제작

---
+++ src/components/ForecastSection.jsx
@@ -0,0 +1,9 @@
+// 5일 예보
+
+import React from 'react'
+
+export default function ForecastSection() {
+  return (
+    <div>ForecastSection</div>
+  )
+}
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,18 +1,21 @@
 // detail page
 
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import WeatherCard from '../components/WeatherCard';
 
 export default function Detail() {
+  const {city} = useParams();
+
   return (
-    <div>detail</div>
-    // 데이터 불러오기 전에 Skeleton UI 적용
-    
-    // 페이지 제목: 검색결과 (도시 이름) 동적 렌더링
+    <div className='flex flex-col mx-auto w-[90%]'>
+      <h1 className="text-3xl font-bold mt-6">{city}</h1>
 
-    // 현재 날씨 정보: 날씨 상태(맑음, 흐림 등), 현재 온도, 체감 온도, 습도, 풍속
-    // 날씨 아이콘이 현재 날씨에 따라 변경됨 
-    // (OpenWeather API 제공 아이콘, 혹은 react-icons 라이브러리)
+      {/* 현재 날씨 */}
+      <WeatherCard city={city}/>
 
-    // 페이지 하단 → 5일 예보: 버튼 클릭시 아코디언처럼 펼쳐지는 구조
+      {/* 5일 예보 - 이후 추가할 예정 */}
+      {/* <ForecastSection city={city} /> */}
+    </div>
   )
 }
