068cb61e416343960646e93f53df5ed07f8e61fc
hjinn0813 <hjc3790@gmail.com>
2026-02-19 13:36:20 +0900

    fix: resolve ESLint and Vite build configuration issues

--- eslint.config.js
+++ eslint.config.js
@@ -5,12 +5,20 @@ import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     ignores: ["dist", "node_modules"],
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
+    // nodejs
+    files: ["*.config.{js,cjs}"],
+    languageOptions: {
+      globals: globals.node,
+    }
+  },
+  {
+    // react ts
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       globals: globals.browser,
@@ -24,11 +32,17 @@ export default [
       react,
       "react-hooks": reactHooks,
     },
+    settings: {
+      react: {
+        version: "detect",
+      }
+    },
     rules: {
       ...react.configs.flat.recommended.rules,
       ...reactHooks.configs.recommended.rules,
 
       "react/react-in-jsx-scope": "off", // Vite/React 17+ 필요 없음
+      "react-hooks/set-state-in-effect": "off", // 신규 react hook 규칙 끄기
     },
   },
 ];
